1fa55d2e88c92307e156274023a06e1f55d0a35e
ekke020 <ekman-johan020@hotmail.com>
2021-09-26 22:16:47 +0200

    JLabel that paints the Tetris logo

---
+++ src/gui/menu/TetrisLogo.java
@@ -0,0 +1,33 @@
+package gui.menu;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class TetrisLogo extends JLabel {
+
+    private BufferedImage image;
+    private final String imagePath = "assets/The_Tetris_Company_logo.png";
+
+
+    public TetrisLogo() {
+        setMaximumSize(new Dimension(373, 259));
+        setPreferredSize(new Dimension(373, 259));
+        try {
+            image = ImageIO.read(new File(imagePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+    }
+}
