459b3c855d9038fbeb3a30dde22839e0e3ebba32
ekke020 <ekman-johan020@hotmail.com>
2021-09-26 22:09:36 +0200

    Changed the color of the button

--- src/gui/menu/MenuButton.java
+++ src/gui/menu/MenuButton.java
@@ -15,9 +15,12 @@ public class MenuButton extends JButton implements ChangeListener {
         setFocusPainted(false);
         setOpaque(true);
         setRolloverEnabled(true);
+        setBorder(BorderFactory.createEmptyBorder());
+
         setBackground(new Color(107, 196, 255, 255));
         setForeground(new Color(229, 232, 255, 200));
         setFont(new Font("Serif", Font.BOLD, 26));
+
         addChangeListener(this);
         KeyBinder.removeKeyBinding(this, KeyEvent.VK_SPACE, "none", false);
     }
@@ -25,9 +28,9 @@ public class MenuButton extends JButton implements ChangeListener {
     @Override
     public void stateChanged(ChangeEvent e) {
         if (getModel().isPressed()) {
-            setBackground(new Color(19, 33, 171, 224));
+            setBackground(new Color(70, 140, 182, 255));
         } else if (getModel().isRollover()) {
-            setBackground(new Color(34, 50, 224, 224));
+            setBackground(new Color(78, 158, 206, 255));
             setForeground(new Color(183, 205, 244, 255));
         } else {
             setBackground(new Color(107, 196, 255, 255));
