ab7c355997aa0d99c31636cfdfd3107463d74920
ekke020 <ekman-johan020@hotmail.com>
2021-09-30 19:34:58 +0200

    Class containg all the colors

---
+++ src/colors/Colors.java
@@ -0,0 +1,24 @@
+package colors;
+
+import java.awt.*;
+
+public class Colors {
+
+    // Panels
+    public static final Color BACKGROUND_COLOR = new Color(64, 64, 64, 255);
+    public static final Color FOREGROUND_COLOR = new Color(230, 230, 230, 255);
+    public static final Color BORDER_COLOR = Color.GRAY;
+
+    // Miscellaneous
+    public static final Color MISC_BACKGROUND = new Color(89, 89, 89, 255);
+
+    // Buttons
+    public static final Color BUTTON_BACKGROUND_COLOR = new Color(102, 102, 102, 255);
+    public static final Color BUTTON_FOREGROUND_COLOR = new Color(179, 179, 179, 255);
+    public static final Color ROLLOVER_BACKGROUND_COLOR = new Color(77, 77, 77, 255);
+    public static final Color ROLLOVER_FOREGROUND_COLOR = new Color(242, 242, 242, 255);
+    public static final Color PRESSED_COLOR = new Color(26, 26, 26, 255);
+
+
+
+}
