e809194b59797bf86c67f1c4fdabc696d268d313
ekke020 <ekman-johan020@hotmail.com>
2021-09-30 18:29:04 +0200

    Added a return button and a panel

--- src/gui/frames/SettingsFrame.java
+++ src/gui/frames/SettingsFrame.java
@@ -1,12 +1,60 @@
 package gui.frames;
 
+import gui.menu.MenuButton;
+import gui.menu.MenuListener;
+import gui.menu.settings.BindingPanel;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class OptionsFrame extends JPanel {
+public class SettingsFrame extends JPanel {
 
-    public OptionsFrame() {
-        setBackground(Color.BLACK);
+    private MenuListener menuListener;
+    private GridBagConstraints gc;
+
+    public SettingsFrame() {
+        setBackground(new Color(64, 64, 64));
+        setPreferredSize(new Dimension(784,961));
+
+        setLayout(new GridBagLayout());
+
+        addBindingPanel();
+        addBackButton();
     }
 
+    private void addBindingPanel() {
+        BindingPanel bindingPanel = new BindingPanel();
+        gc = new GridBagConstraints();
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+
+        gc.anchor = GridBagConstraints.CENTER;
+        gc.fill = GridBagConstraints.NONE;
+
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        add(bindingPanel, gc);
+    }
+
+    private void addBackButton() {
+        MenuButton backButton = new MenuButton("Back");
+        backButton.addActionListener(e -> menuListener.formEventOccurred(null));
+        gc = new GridBagConstraints();
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+
+        gc.anchor = GridBagConstraints.LAST_LINE_START;
+        gc.fill = GridBagConstraints.NONE;
+
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        add(backButton, gc);
+    }
+    public void setMenuListener(MenuListener menuListener) {
+        this.menuListener = menuListener;
+    }
 }
