1fcf0e4dc2ccc4360c97f6c9ea1ced98f97cc955
ekke020 <ekman-johan020@hotmail.com>
2021-10-03 19:42:14 +0200

    Added a componentListener

--- src/gui/frames/SettingsFrame.java
+++ src/gui/frames/SettingsFrame.java
@@ -1,6 +1,7 @@
 package gui.frames;
 
 import colors.Colors;
+import gui.ComponentResizeListener;
 import gui.menu.MenuButton;
 import gui.menu.MenuListener;
 import gui.menu.settings.BindingPanel;
@@ -8,23 +9,33 @@ import gui.menu.settings.BindingPanel;
 import javax.swing.*;
 import java.awt.*;
 
+// TODO: Add different color modes.
+
 public class SettingsFrame extends JPanel {
 
     private MenuListener menuListener;
+    private MenuButton backButton;
+    private BindingPanel bindingPanel;
     private GridBagConstraints gc;
 
-    public SettingsFrame() {
+    public SettingsFrame(int width, int height) {
         setBackground(Colors.BACKGROUND_COLOR);
-        setPreferredSize(new Dimension(784,961));
+        setPreferredSize(new Dimension(width, height));
 
         setLayout(new GridBagLayout());
 
-        addBindingPanel();
-        addBackButton();
+        addBindingPanel(width / 3, height / 3);
+        addBackButton(width / 3, height / 16);
+        addComponentListener(new ComponentResizeListener() {
+            @Override
+            public void resizeTimedOut() {
+                setComponentSizes();
+            }
+        });
     }
 
-    private void addBindingPanel() {
-        BindingPanel bindingPanel = new BindingPanel();
+    private void addBindingPanel(int width, int height) {
+        bindingPanel = new BindingPanel(width, height);
         gc = new GridBagConstraints();
 
         gc.gridx = 0;
@@ -35,12 +46,11 @@ public class SettingsFrame extends JPanel {
 
         gc.weightx = 1;
         gc.weighty = 0.1;
-
         add(bindingPanel, gc);
     }
 
-    private void addBackButton() {
-        MenuButton backButton = new MenuButton("Back");
+    private void addBackButton(int buttonWidth, int buttonHeight) {
+        backButton = new MenuButton("Back",buttonWidth, buttonHeight);
         backButton.addActionListener(e -> menuListener.formEventOccurred(null));
         gc = new GridBagConstraints();
 
@@ -50,6 +60,7 @@ public class SettingsFrame extends JPanel {
         gc.anchor = GridBagConstraints.LAST_LINE_START;
         gc.fill = GridBagConstraints.NONE;
 
+        gc.gridwidth = GridBagConstraints.REMAINDER;
         gc.weightx = 1;
         gc.weighty = 0.1;
 
@@ -58,4 +69,13 @@ public class SettingsFrame extends JPanel {
     public void setMenuListener(MenuListener menuListener) {
         this.menuListener = menuListener;
     }
+
+    private void setComponentSizes() {
+        // Update all the component sizes here.
+        System.out.println("SettingsFrame");
+        backButton.setPreferredSize(new Dimension(getWidth() / 3, getHeight() / 16));
+        bindingPanel.setPreferredSize(new Dimension(getWidth() / 3, getHeight() / 3));
+        SwingUtilities.updateComponentTreeUI(this);
+    }
 }
+
