8d1f97f41813eef226fcc3c8cb8e07ec4f513df5
ekke020 <ekman-johan020@hotmail.com>
2021-10-03 19:46:03 +0200

    Made the Foreground dynamic with the size of the button

--- src/gui/menu/MenuButton.java
+++ src/gui/menu/MenuButton.java
@@ -11,18 +11,18 @@ import java.awt.event.KeyEvent;
 
 public class MenuButton extends JButton implements ChangeListener {
 
-    public MenuButton(String text) {
+    public MenuButton(String text, int width, int height) {
         super(text);
         setFocusPainted(false);
         setOpaque(true);
         setRolloverEnabled(true);
         setBorder(BorderFactory.createEmptyBorder());
+        setPreferredSize(new Dimension(width, height));
 
         setBackground(Colors.BUTTON_BACKGROUND_COLOR);
         setForeground(Colors.BUTTON_FOREGROUND_COLOR);
 
         UIManager.put("Button.select", Colors.PRESSED_COLOR);
-        setFont(new Font("Serif", Font.BOLD, 26));
 
         addChangeListener(this);
         KeyBinder.removeKeyBinding(this, KeyEvent.VK_SPACE, "none", false);
@@ -40,6 +40,13 @@ public class MenuButton extends JButton implements ChangeListener {
             setBackground(Colors.BUTTON_BACKGROUND_COLOR);
             setForeground(Colors.BUTTON_FOREGROUND_COLOR);
         }
+    }
+
+    @Override
+    public void setPreferredSize(Dimension preferredSize) {
+        super.setPreferredSize(preferredSize);
+        int size = (int) (preferredSize.getWidth() / 10);
+        setFont(new Font("Serif", Font.BOLD, size));
     }
 
 }
