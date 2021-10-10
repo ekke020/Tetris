247219114498b1c02684f80b483c45d534aa3b94
Johan Ekman <ekman-johan020@hotmail.com>
2021-10-10 20:20:57 +0200

    Added the textfields to an array of TextField

--- src/gui/menu/game/ScoreFields.java
+++ src/gui/menu/game/ScoreFields.java
@@ -5,31 +5,40 @@ import java.awt.*;
 
 public class ScoreFields extends JPanel {
 
+    private final TextField[] textFields = new TextField[6];
 
-    private final TextField levelText;
-    private final TextField scoreText;
-    private final TextField linesText;
 
     public ScoreFields() {
         setLayout(new GridLayout(3, 2));
         setOpaque(false);
 
-        add(new TextField("LEVEL", 4, 2, 4, 0, SwingConstants.LEFT));
-        levelText = new TextField("0", 4, 2, 0, 4, SwingConstants.RIGHT);
-        add(levelText);
+        textFields[0] = new TextField("LEVEL", 4, 2, 4, 0, SwingConstants.LEFT);
+        add(textFields[0]);
+        textFields[1] = new TextField("0", 4, 2, 0, 4, SwingConstants.RIGHT);
+        add(textFields[1]);
 
-        add(new TextField("SCORE", 2, 2, 4, 0, SwingConstants.LEFT));
-        scoreText = new TextField("0", 2, 2, 0, 4, SwingConstants.RIGHT);
-        add(scoreText);
+        textFields[2] = new TextField("SCORE", 2, 2, 4, 0, SwingConstants.LEFT);
+        add(textFields[2]);
+        textFields[3] = new TextField("0", 2, 2, 0, 4, SwingConstants.RIGHT);
+        add(textFields[3]);
 
-        add(new TextField("LINES", 2, 4, 4, 0, SwingConstants.LEFT));
-        linesText = new TextField("0", 2, 4, 0, 4, SwingConstants.RIGHT);
-        add(linesText);
+        textFields[4] = new TextField("LINES", 2, 4, 4, 0, SwingConstants.LEFT);
+        add(textFields[4]);
+        textFields[5] = new TextField("0", 2, 4, 0, 4, SwingConstants.RIGHT);
+        add(textFields[5]);
     }
 
     public void updateFields(int level, int score, int lines) {
-        levelText.setText(String.valueOf(level));
-        scoreText.setText(String.valueOf(score));
-        linesText.setText(String.valueOf(lines));
+        textFields[1].setText(String.valueOf(level));
+        textFields[3].setText(String.valueOf(score));
+        textFields[5].setText(String.valueOf(lines));
+    }
+    @Override
+    public void setPreferredSize(Dimension preferredSize) {
+        super.setPreferredSize(preferredSize);
+        System.out.println(getWidth());
+        for (TextField textField : textFields) {
+//            textField.setColumns();
+        }
     }
 }
