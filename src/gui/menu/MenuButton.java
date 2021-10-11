9cba6634786576fdba76bda2dc72871c6258da4d
Johan Ekman <ekman-johan020@hotmail.com>
2021-10-11 22:19:19 +0200

    Fixed a bug that caused the spin key to loose focus after alt tabbing from the game

--- src/gui/menu/MenuButton.java
+++ src/gui/menu/MenuButton.java
@@ -26,6 +26,7 @@ public class MenuButton extends JButton implements ChangeListener {
 
         addChangeListener(this);
         KeyBinder.removeKeyBinding(this, KeyEvent.VK_SPACE, "none", false);
+        KeyBinder.removeKeyBinding(this, KeyEvent.VK_SPACE, "none", true);
     }
 
     @Override
