c41e94ec99819449234cdb8dcf5968c51c6f04a0
ekke020 <ekman-johan020@hotmail.com>
2021-10-11 20:25:39 +0200

    Updated the class so it can play different sounds

--- src/sound/AudioPlayer.java
+++ src/sound/AudioPlayer.java
@@ -3,27 +3,57 @@ package sound;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class AudioPlayer {
 
-    private static Clip clip;
+    private Clip clip;
+    private final boolean isLoop;
+    private final Path path;
+    private final float volume;
 
-    /*
-     * Early test of implementing sound.
-     */
-    public static void play() {
-        try {
-            AudioInputStream input = AudioSystem.getAudioInputStream(new File("assets/sound/Tetris_Game_Sound.wav"));
-            clip=AudioSystem.getClip();
+    public AudioPlayer(Path path, boolean isLoop, float volume) {
+        this.isLoop = isLoop;
+        this.path = path;
+        this.volume = volume;
+    }
+
+    public void play() {
+        try (AudioInputStream input = AudioSystem.getAudioInputStream(new File(path.toString()))) {
+            clip = AudioSystem.getClip();
             clip.open(input);
+            setVolume(volume);
             clip.start();
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (UnsupportedAudioFileException e) {
+            if (isLoop) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
+
+    public void pause() {
+        clip.stop();
+    }
+
+    public void start() {
+        clip.start();
+        if (isLoop) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
+
+    public void close() {
+        clip.close();
+    }
+
+    private void setVolume(float volume) {
+        if (volume < 0f || volume > 1f)
+            throw new IllegalArgumentException("Volume not valid: " + volume);
+
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(volume));
+    }
+
 }
