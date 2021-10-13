36d110ed04054be5ca87302c05f58f32cecf6ced
ekke020 <ekman-johan020@hotmail.com>
2021-10-13 16:12:49 +0200

    A JSlider component class

---
+++ src/gui/menu/settings/SoundSlider.java
@@ -0,0 +1,28 @@
+package gui.menu.settings;
+
+import colors.Colors;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+import static colors.Colors.*;
+
+public class SoundSlider extends JSlider {
+
+    public SoundSlider(int width, String title) {
+        super(JSlider.HORIZONTAL);
+        setPreferredSize(new Dimension(width, 35));
+        setBackground(BACKGROUND_COLOR);
+        Font titleFont = new Font("Serif", Font.BOLD, width / 20);
+        Border emptyBorder = BorderFactory.createEmptyBorder();
+        setBorder(BorderFactory.createTitledBorder(emptyBorder, title,
+                TitledBorder.LEFT, TitledBorder.ABOVE_TOP, titleFont, Colors.FOREGROUND_COLOR));
+        addChangeListener(e -> {
+            int soundLevel = getValue();
+            System.out.println(soundLevel);
+        });
+    }
+
+}
