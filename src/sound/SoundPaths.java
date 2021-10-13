8abf5b50ad6c63bcf93fd151175505d028b0d8d4
ekke020 <ekman-johan020@hotmail.com>
2021-10-13 13:16:58 +0200

    Removed the volume variable

--- src/sound/SoundPaths.java
+++ src/sound/SoundPaths.java
@@ -5,32 +5,26 @@ import java.nio.file.Paths;
 
 public enum SoundPaths {
 
-    GAME_OVER("assets/sound/Game_Over.wav", 0.2f),
-    TETRIS_GAME_SOUND("assets/sound/Tetris_Game_Sound.wav", 0.2f),
-    TETRIS_MENU_SOUND("assets/sound/Tetris_Menu_Sound.wav", 0.2f),
-    PAUSE("assets/sound/Pause.wav", 0.6f),
-    TETRIS("assets/sound/Tetris.wav", 0.6f),
-    LINE_CLEARED("assets/sound/Line_Cleared.wav", 0.6f),
-    TETROMINO_LANDING("assets/sound/Tetromino_Landing.wav", 0.6f),
-    TETROMINO_MOVE("assets/sound/Tetromino_Move.wav", 0.6f),
-    TETROMINO_MOVE_DENIED("assets/sound/Tetromino_Move_Denied.wav", 0.6f),
-    TETROMINO_SPIN("assets/sound/Tetromino_Spin.wav", 0.6f),
-    LEVEL_UP("assets/sound/Level_Up.wav", 0.6f);
+    GAME_OVER_SOUND("assets/sound/Game_Over.wav"),
+    TETRIS_GAME_SOUND("assets/sound/Tetris_Game_Sound.wav"),
+    TETRIS_MENU_SOUND("assets/sound/Tetris_Menu_Sound.wav"),
+    PAUSE_SOUND("assets/sound/Pause.wav"),
+    TETRIS_SOUND("assets/sound/Tetris.wav"),
+    LINE_CLEARED_SOUND("assets/sound/Line_Cleared.wav"),
+    TETROMINO_LANDING_SOUND("assets/sound/Tetromino_Landing.wav"),
+    TETROMINO_MOVE_SOUND("assets/sound/Tetromino_Move.wav"),
+    TETROMINO_MOVE_DENIED_SOUND("assets/sound/Tetromino_Move_Denied.wav"),
+    TETROMINO_SPIN_SOUND("assets/sound/Tetromino_Spin.wav"),
+    LEVEL_UP_SOUND("assets/sound/Level_Up.wav");
 
     private final Path path;
-    public final float volume;
 
     public Path getPath() {
         return path;
     }
 
-    public float getVolume() {
-        return volume;
-    }
-
-    SoundPaths(String path, float volume) {
+    SoundPaths(String path) {
         this.path = Paths.get(path);
-        this.volume = volume;
     }
 
 }
