08cff377379606d045b06b9721979c74d98c0c62
ekke020 <ekman-johan020@hotmail.com>
2021-10-13 13:15:58 +0200

    Changed the way music is loaded, its now stored as a bit array

--- src/sound/AudioPlayer.java
+++ src/sound/AudioPlayer.java
@@ -1,54 +1,109 @@
 package sound;
 
 import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
+import java.util.HashMap;
+
+import static sound.SoundPaths.*;
 
 public class AudioPlayer {
 
-    private Clip clip;
-    private final boolean isLoop;
-    private final Path path;
-    private final float volume;
+    private static final HashMap<Integer, byte[]> soundMap = new HashMap<>();
+    private static Clip backgroundMusic;
 
-    public AudioPlayer(Path path, boolean isLoop, float volume) {
-        this.isLoop = isLoop;
-        this.path = path;
-        this.volume = volume;
+    public static final int GAME = 0;
+    public static final int MENU = 1;
+
+    public static final int PAUSE = 2;
+    public static final int GAME_OVER = 3;
+
+    public static final int MOVE = 4;
+    public static final int MOVE_DENIED = 5;
+    public static final int SPIN = 6;
+    public static final int TETROMINO_LANDING = 7;
+
+    public static final int TETRIS = 8;
+    public static final int LINE_CLEARED = 9;
+    public static final int LEVEL_UP = 10;
+
+    static {
+        soundMap.put(0, loadClipByteArray(TETRIS_GAME_SOUND.getPath()));
+        soundMap.put(1, loadClipByteArray(TETRIS_MENU_SOUND.getPath()));
+        soundMap.put(2, loadClipByteArray(PAUSE_SOUND.getPath()));
+        soundMap.put(3, loadClipByteArray(GAME_OVER_SOUND.getPath()));
+        soundMap.put(4, loadClipByteArray(TETROMINO_MOVE_SOUND.getPath()));
+        soundMap.put(5, loadClipByteArray(TETROMINO_MOVE_DENIED_SOUND.getPath()));
+        soundMap.put(6, loadClipByteArray(TETROMINO_SPIN_SOUND.getPath()));
+        soundMap.put(7, loadClipByteArray(TETROMINO_LANDING_SOUND.getPath()));
+        soundMap.put(8, loadClipByteArray(TETRIS_SOUND.getPath()));
+        soundMap.put(9, loadClipByteArray(LINE_CLEARED_SOUND.getPath()));
+        soundMap.put(10, loadClipByteArray(LEVEL_UP_SOUND.getPath()));
     }
 
-    public void play() {
-        try (AudioInputStream input = AudioSystem.getAudioInputStream(new File(path.toString()))) {
-            clip = AudioSystem.getClip();
-            clip.open(input);
-            setVolume(volume);
-            clip.start();
-            if (isLoop) {
-                clip.loop(Clip.LOOP_CONTINUOUSLY);
-            }
-        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
+    public static void play(int sound) {
+        Clip clip = getClipFromByteArray(soundMap.get(sound));
+        if (clip == null) {
+            System.out.println("Returned");
+            return;
         }
-    }
-
-    public void pause() {
-        clip.stop();
-    }
-
-    public void start() {
-        clip.start();
-        if (isLoop) {
+        if (sound == 0 || sound == 1) {
+            switchBackgroundMusic(clip);
+            setVolume(clip, 0.2f);
             clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } else {
+            setVolume(clip, 0.8f);
         }
+        clip.start();
     }
 
-    public void close() {
-        clip.close();
+    private static void switchBackgroundMusic(Clip sound) {
+        if (backgroundMusic != null) {
+            backgroundMusic.close();
+        }
+        backgroundMusic = sound;
     }
 
-    private void setVolume(float volume) {
+    public static void pauseBackgroundMusic() {
+        backgroundMusic.stop();
+    }
+
+    public static void resumeBackgroundMusic() {
+        backgroundMusic.start();
+        backgroundMusic.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    private static byte[] loadClipByteArray(Path path) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try(BufferedInputStream in = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+            int read;
+            byte[] buff = new byte[1024];
+            while ((read = in.read(buff)) > 0)
+            {
+                out.write(buff, 0, read);
+            }
+            out.flush();
+            return out.toByteArray();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static Clip getClipFromByteArray(byte[] clipByte) {
+        try (AudioInputStream input = AudioSystem.getAudioInputStream(new ByteArrayInputStream(clipByte))) {
+            DataLine.Info info = new DataLine.Info(Clip.class, input.getFormat());
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(input);
+            return clip;
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    //TODO: Fix the volume of each Clip
+    private static void setVolume(Clip clip, float volume) {
         if (volume < 0f || volume > 1f)
             throw new IllegalArgumentException("Volume not valid: " + volume);
 
