467b8018fafd5ca02ab70900a0a5fb9b91f265c1
Johan Ekman <ekman-johan020@hotmail.com>
2021-11-02 14:28:08 +0100

    A new tetromino shape that is used when the game is over

---
+++ src/tetromino/EndOfGameShape.java
@@ -0,0 +1,10 @@
+package tetromino;
+
+import colors.Colors;
+
+public class EndOfGameShape extends Tetromino {
+
+    public EndOfGameShape() {
+        this.color = Colors.GAME_OVER_COLOR;
+    }
+}
